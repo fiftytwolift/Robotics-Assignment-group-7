0ca410497e829e0e8571696f3dd1879b5f9e7a75
Samuel Wong <wongsamuelhinsum@gmail.com>
2020-04-26 18:05:11 +1000

    Added a general trajGen function that is useful to part 2 and 3
    a

--- Assignment 3/plot_displacement.m
+++ Assignment 3/plot_displacement.m
@@ -5,11 +5,10 @@ if (length(segmentCoeff)/4 ~= length(tfinal))
     warning('The input is not valid')
     return;
 end
-segmentCoeff
 %% plot the cubic spline
 % get the array for t
 t = 0;
-delta = 0.1;
+delta = 0.01;
 for i = 1:length(tfinal)
     t = [t (t(end)+delta):delta:tfinal(i)];
 end
@@ -18,12 +17,11 @@ cur_segment = 1;
 total_segment = length(segmentCoeff)/4;
 for time_index = 1: length(t)
     if t(time_index) > tfinal(cur_segment) + delta
-        fprintf( 't is %f \n',t(time_index));
         cur_segment = cur_segment + 1;
     end
     displacement(time_index) = [zeros(1,(cur_segment-1)*4) t(time_index)^3 t(time_index)^2 t(time_index)^1 1 zeros(1,(total_segment-cur_segment)*4)]*segmentCoeff;
 end
-plot(t,displacement)
+plot(t,displacement);
 title('displacement vs time')
 xlabel('time/s')
 ylabel('displacement / m')
