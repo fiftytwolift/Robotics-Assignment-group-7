d0752c67aa211958b27ae2eba80923f57bda78d3
Samuel Wong <wongsamuelhinsum@gmail.com>
2020-04-27 15:06:20 +1000

    Added elbow up consideration

--- Assignment 3/plot_robot.m
+++ Assignment 3/plot_robot.m
@@ -1,9 +1,11 @@
 function plot_robot(Q1,Q2)
+% length of the arm
 L1 = 9;
 L2 = 9;
-division = 100;
+% points of joints at the robot
 mid_pt = [L1*cosd(Q1),L1*sind(Q1)];
 end_effector = mid_pt+[L2*cosd(Q1+Q2),L2*sind(Q1+Q2)];
+
 x = [0 mid_pt(1) end_effector(1)];
 y = [0 mid_pt(2) end_effector(2)];
 hold on
--- Assignment 3/two_arm_IK.m
+++ Assignment 3/two_arm_IK.m
@@ -3,5 +3,9 @@ function [Q1,Q2] = two_arm_IK(x,y)
 L1 = 9;
 L2 = 9;
 % from lecture 07 slide 11 - 13
+% elbow down
 Q2 = acosd((x^2+y^2 - (L1^2+L2^2))/(2*L1*L2));
 Q1 = atand(y/x) - asind((L2*sind(180-Q2))/sqrt(x^2+y^2));
+% elbow up
+% Q2 = -acosd((x^2+y^2 - (L1^2+L2^2))/(2*L1*L2));
+% Q1 = atand(y/x) + asind((L2*sind(180+Q2))/sqrt(x^2+y^2));
